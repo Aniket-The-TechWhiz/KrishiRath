5a1057203e7fee13493ee625aed8a9b10a100183
Aniket-The-TechWhiz <aniketyelameli@gmail.com>
2026-01-22 21:25:26 +0530

    deleted some files

--- model/croprecommendation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const planSchema = new mongoose.Schema({
-    planSchemaId: { type: String, required: true },
-});
-
-const Plan = mongoose.model('Plan', planSchema);
-
-module.exports = Plan;
